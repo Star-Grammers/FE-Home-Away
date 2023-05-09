ed00c0bd0fc6c47645c3a37ac6c108da65d09b51
Maira Medrano <mairamedrano28@gmail.com>
2023-05-09 14:22:03 -0500

    Add login component

---
+++ my-react-app/src/Login.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // handle login logic here
+    };
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
